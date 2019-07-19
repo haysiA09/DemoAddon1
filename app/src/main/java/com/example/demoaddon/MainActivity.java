835f1ed131dec2de37297847099058af417bc395
16022877 <16022877@myrp.edu.sg>
2019-07-19 12:00:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demoaddon/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.demoaddon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv=findViewById(R.id.iv);
+//        iv.setImageResource(R.mipmap.ic_launcher);
+        String imageUrl = "http://jsibbold.github.io/zoomage/";
+        Picasso.with(this).load(imageUrl).into(iv);
+
+    }
+}
